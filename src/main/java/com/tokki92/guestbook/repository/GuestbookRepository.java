757b1a00a2c88a1acb91f0f1f57961df54c520d8
tokki92 <pyw5420@naver.com>
2022-01-12 13:27:56 +0900

    GuestbookRepository 인터페이스 정의

---
+++ src/main/java/com/tokki92/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,7 @@
+package com.tokki92.guestbook.repository;
+
+import com.tokki92.guestbook.entity.Guestbook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+}
