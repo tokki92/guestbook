665879910232d5b1efbf4eec8e3c85487c83770e
tokki92 <pyw5420@naver.com>
2022-01-12 12:47:13 +0900

    목록 처리용 DTO 정의

---
+++ src/main/java/com/tokki92/guestbook/dto/PageResultDTO.java
@@ -0,0 +1,19 @@
+package com.tokki92.guestbook.dto;
+
+import lombok.Data;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Data
+public class PageResultDTO<DTO, EN> {
+
+    // DTO 리스트
+    private List<DTO> dtoList;
+
+    public PageResultDTO(Page<EN> result, Function<EN, DTO> fn) {
+        dtoList = result.stream().map(fn).collect(Collectors.toList());
+    }
+}
