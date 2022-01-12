abc38d209ef997ffaedf564de33b5a3f963d2fa7
tokki92 <pyw5420@naver.com>
2022-01-12 13:25:49 +0900

    목록 처리용 핸들러 메서드 정의

---
+++ src/main/java/com/tokki92/guestbook/controller/GuestbookController.java
@@ -0,0 +1,32 @@
+package com.tokki92.guestbook.controller;
+
+import com.tokki92.guestbook.dto.PageRequestDTO;
+import com.tokki92.guestbook.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/guestbook")
+@Log4j2
+@RequiredArgsConstructor
+public class GuestbookController {
+
+    private final GuestbookService service;
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+        log.info("list.........." + pageRequestDTO);
+
+        model.addAttribute("result", service.getList(pageRequestDTO));
+
+    }
+}
